6dc63a531014bce961e054698ad4ff1a8962da66
unknown <lmmotel@donbosco.edu.ph>
2022-07-25 21:27:56 -0700

    Adding SQL File

--- pocketguide.sql
+++ pocketguide.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 12, 2022 at 09:11 PM
+-- Generation Time: Jul 26, 2022 at 06:26 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -24,43 +24,43 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_accounts`
+-- Table structure for table `tbl_announcement`
 --
 
-CREATE TABLE `user_accounts` (
-  `uid` int(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `fullname` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `user_type` varchar(100) NOT NULL
+CREATE TABLE `tbl_announcement` (
+  `a_id` int(100) NOT NULL,
+  `date_posted` varchar(200) NOT NULL,
+  `a_author` varchar(200) NOT NULL,
+  `a_subject` varchar(200) NOT NULL,
+  `a_content` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user_accounts`
+-- Dumping data for table `tbl_announcement`
 --
 
-INSERT INTO `user_accounts` (`uid`, `email`, `fullname`, `password`, `user_type`) VALUES
-(1, 'motel@gmail.com', 'liezelmotel', '123', '1');
+INSERT INTO `tbl_announcement` (`a_id`, `date_posted`, `a_author`, `a_subject`, `a_content`) VALUES
+(2, 'Sunday, 28. July 1895 16:18', '2', 'Typhoon', 'No Classes');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `user_accounts`
+-- Indexes for table `tbl_announcement`
 --
-ALTER TABLE `user_accounts`
-  ADD PRIMARY KEY (`uid`);
+ALTER TABLE `tbl_announcement`
+  ADD PRIMARY KEY (`a_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `user_accounts`
+-- AUTO_INCREMENT for table `tbl_announcement`
 --
-ALTER TABLE `user_accounts`
-  MODIFY `uid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `tbl_announcement`
+  MODIFY `a_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
